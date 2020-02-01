273a57cb5cf93c2a772e2804601c2f5ce1791d50
Loarlam <l9m_pam@live.ru>
2020-02-01 21:13:24 +0300

    Add project files.

---
+++ Addition task/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Addition_task
+{
+    class User
+    {
+        string login, name, surname;
+        int age;
+        readonly string date;
+        public User(string login, string name, string surname, int age, string date)
+        {
+            this.date = date;
+            this.login = login;
+            this.name = name;
+            this.surname = surname;
+            this.age = age;
+        }
+
+        public void InformationOfPeople()
+        {
+            Console.WriteLine("Логин: {0}\nИмя: {1}\nФамилия: {2}\nВозраст: {3}\nДата заполнения анкеты: {4}", login, name, surname, age, date);
+        }
+
+    }
+}
